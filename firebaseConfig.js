b7e2c89b0dffee3a5fb14ac9f0db29bd41d9701f
UmeAimen <168212896+umeaimen337@users.noreply.github.com>
2025-04-19 18:22:57 +0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAiLsAU2lU7lI72eu1ZSnjUiJvUiSNnmOg",
-    authDomain: "ai-based-interview-app.firebaseapp.com",
-    projectId: "ai-based-interview-app",
-    storageBucket: "ai-based-interview-app.firebasestorage.app",
-    messagingSenderId: "276197556048",
-    appId: "1:276197556048:web:9e99c1887f4d04e3ef3655"
+    apiKey: "abc",
+    authDomain: "abc",
+    projectId: "abc",
+    storageBucket: "ab",
+    messagingSenderId: "abc",
+    appId: "abc"
 };
 
 // Initialize Firebase
